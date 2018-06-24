316ae1684e040ba1d3c48959853caf440b780857
rien333 <rien334@gmail.com>
2018-06-24 14:39:58 +0200

    Change size a little

--- image_generator.py
+++ image_generator.py
@@ -297,7 +297,7 @@ def generate_set(fidx, nfiles, cat, thresh=0.5):
 # _, obj_f = single_obj()
 b = background_im()
 for i in range(4):
-    im, _ = generate_image(i, thresh=1.0, background=b, total_size=5000)
+    im, _ = generate_image(i, thresh=1.0, background=b, total_size=5002)
     cv2.imwrite("/tmp/test%s.png" % (i), im)
 
 # generate(4000, 4000, cat=4, thresh=0.7)
